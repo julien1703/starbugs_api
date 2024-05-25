4b3f5f861a336f42bf705eaf7e8eb11b58636f60
Julien1703 <128695505+Julien1703@users.noreply.github.com>
2024-05-25 19:29:52 +0200

    test all stars

--- index.js
+++ index.js
@@ -37,14 +37,19 @@ app.get('/constellation', async (req, res) => {
     const { constellation } = req.query;
     console.log(`calling /constellation for: ${constellation}`);
     try {
-        const stars = await Star.find({
-            con: { $regex: constellation, $options: 'i' },
-            $or: [
-                { bay: { $nin: [null, ''] } },
-                { flam: { $nin: [null, ''] } },
-                { proper: { $nin: [null, ''] } }
-            ]
-        }).sort({ mag: 1 });
+        let query = {};
+        if (constellation) {
+            query = {
+                con: { $regex: constellation, $options: 'i' },
+                $or: [
+                    { bay: { $nin: [null, ''] } },
+                    { flam: { $nin: [null, ''] } },
+                    { proper: { $nin: [null, ''] } }
+                ]
+            };
+        }
+        console.log('Query:', JSON.stringify(query));
+        const stars = await Star.find(query).sort({ mag: 1 });
         res.json(stars);
         console.log(`Found stars: ${stars.length}`);
     } catch (error) {
