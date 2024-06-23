c7548fe506fd53bed17192ac0e72de1bb7208d45
FR4NZP <kidpfisto@gmail.com>
2024-06-23 13:10:02 +0200

    nur key im text

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const axios = require('axios');
+// const axios = require('axios');
 const app = express();
 const cors = require('cors');
 
@@ -134,36 +134,36 @@ function getConstellationConnections(constellation, stars) {
     return connections.filter(connection => starsMap[connection.from] && starsMap[connection.to]);
 }
 
-// Endpunkt zum Abrufen des API-Schlüssels
-app.get('/get-api-key', (req, res) => {
-    res.json({ apiKey: openAiKey });
-});
+// // Endpunkt zum Abrufen des API-Schlüssels
+// app.get('/get-api-key', (req, res) => {
+//     res.json({ apiKey: openAiKey });
+// });
 
-// Endpunkt zum Generieren von Text basierend auf dem Sternzeichen
-app.post('/api/generate-text', async (req, res) => {
-    const { starsign } = req.body;
+// // Endpunkt zum Generieren von Text basierend auf dem Sternzeichen
+// app.post('/api/generate-text', async (req, res) => {
+//     const { starsign } = req.body;
 
-    try {
-        const response = await axios.post(
-            'https://api.openai.com/v1/engines/davinci-codex/completions',
-            {
-                prompt: `Gib mir eine spezifische Beschreibung für das Sternzeichen ${starsign}.`,
-                max_tokens: 100,
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${openAiKey}`,
-                },
-            }
-        );
+//     try {
+//         const response = await axios.post(
+//             'https://api.openai.com/v1/engines/davinci-codex/completions',
+//             {
+//                 prompt: `Gib mir eine spezifische Beschreibung für das Sternzeichen ${starsign}.`,
+//                 max_tokens: 100,
+//             },
+//             {
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'Authorization': `Bearer ${openAiKey}`,
+//                 },
+//             }
+//         );
 
-        res.json({ text: response.data.choices[0].text.trim() });
-    } catch (error) {
-        console.error('Fehler beim Generieren des Textes:', error);
-        res.status(500).json({ error: 'Fehler bei der Textgenerierung' });
-    }
-});
+//         res.json({ text: response.data.choices[0].text.trim() });
+//     } catch (error) {
+//         console.error('Fehler beim Generieren des Textes:', error);
+//         res.status(500).json({ error: 'Fehler bei der Textgenerierung' });
+//     }
+// });
 
 app.get('/', (req, res) => {
     res.send(`Welcome to the Starbugs API!neu ${openAiKey} `);
